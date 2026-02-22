4d650f8df444b56065744aa1a0896a374436e051
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-02-22 19:23:44 +0100

    java change

--- MyTopDownShooter/build.gradle.kts
+++ MyTopDownShooter/build.gradle.kts
@@ -1,1 +1,6 @@
 // TODO: configure multi-module Gradle build
+plugins {
+    id("com.android.application")
+}
+sourceCompatibility = JavaVersion.VERSION_17
+targetCompatibility = JavaVersion.VERSION_17
