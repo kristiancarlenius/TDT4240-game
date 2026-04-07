d38af7276c39669ab8cabfbf1741f3f57fa470a0
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-04-07 19:28:29 +0200

    server hosting

--- LootemShootem/settings.gradle.kts
+++ LootemShootem/settings.gradle.kts
@@ -1,2 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "LootemShootem"
 include("server", "shared", "core", "android", "desktop")
